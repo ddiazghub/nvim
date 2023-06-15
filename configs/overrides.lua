5316ef00507f706a36caafe3fb25c63bdcf613ee
ddiazghub <dedemoya@uninorte.edu.co>
2023-06-15 16:59:51 -0500

    First commit

---
+++ configs/overrides.lua
@@ -0,0 +1,75 @@
+local M = {}
+
+M.treesitter = {
+  ensure_installed = {
+    "vim",
+    "vimdoc",
+    "lua",
+    "html",
+    "css",
+    "javascript",
+    "typescript",
+    "tsx",
+    "c",
+    "markdown",
+    "markdown_inline",
+    "cpp",
+    "python",
+    "toml",
+    "rust",
+    "json"
+  },
+  indent = {
+    enable = true,
+    -- disable = {
+    --   "python"
+    -- },
+  },
+}
+
+M.mason = {
+  ensure_installed = {
+    -- lua stuff
+    "lua-language-server",
+    "stylua",
+
+    -- web dev stuff
+    "css-lsp",
+    "html-lsp",
+    "typescript-language-server",
+    "deno",
+    "prettier",
+
+    -- c/cpp stuff
+    "clangd",
+    "clang-format",
+
+    -- rust stuff
+    "rust-analyzer",
+    "rustfmt",
+
+    -- python stuff
+    "pyright",
+    "mypy",
+    "ruff",
+    "black"
+  },
+}
+
+-- git support in nvimtree
+M.nvimtree = {
+  git = {
+    enable = true,
+  },
+
+  renderer = {
+    highlight_git = true,
+    icons = {
+      show = {
+        git = true,
+      },
+    },
+  },
+}
+
+return M
