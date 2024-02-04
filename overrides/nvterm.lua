4f59a2c05bca556fe77c8f245ce34a7a4b07ce89
ddiazghub <dedemoya@uninorte.edu.co>
2024-02-04 18:11:45 -0500

    Added terminal support for macos

--- overrides/nvterm.lua
+++ overrides/nvterm.lua
@@ -1,5 +1,7 @@
 local function get_shell()
-  if vim.loop.os_uname().sysname == "Linux" then
+  local sysname = vim.loop.os_uname().sysname
+
+  if sysname == "Linux" or sysname == "Darwin" then
     return vim.fn.executable("zsh") == 1 and "zsh" or "bash"
   else
     return vim.fn.executable("pwsh") == 1 and "pwsh" or "powershell"
